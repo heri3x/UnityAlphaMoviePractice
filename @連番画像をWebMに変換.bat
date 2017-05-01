654a2f6b19730287a3f0173923eb488e9dedcf3f
heri3x <heri3x@himehagi.com>
2017-05-02 08:16:23 +0900

    Unityプロジェクトに動画ファイルを追加

--- @連番画像をWebMに変換.bat
+++ @連番画像をWebMに変換.bat
@@ -1,4 +1,5 @@
 @SET FFMPEG_EXE=ffmpeg\bin\ffmpeg.exe
+@SET OUTPUT_DIR=AlphaMoviePractice\Assets\Movies\
 
 @REM -- 動画のビットレート
 @SET BITRATE=3500k
@@ -9,8 +10,8 @@
 @REM -- 自動で代替参照フレーム機能を使う（※透過ムービーでは0を指定）
 @SET USE_AUTO_ALT_REF=0
 
-"%FFMPEG_EXE%" -r %FRAMERATE% -i C88unity-chan_animation/A/png/A_%%5d.png -vb %BITRATE% -auto-alt-ref %USE_AUTO_ALT_REF% -c:v libvpx UnityAnimeA.webm
-"%FFMPEG_EXE%" -r %FRAMERATE% -i C88unity-chan_animation/B/png/B_%%5d.png -vb %BITRATE% -auto-alt-ref %USE_AUTO_ALT_REF% -c:v libvpx UnityAnimeB.webm
+"%FFMPEG_EXE%" -r %FRAMERATE% -i C88unity-chan_animation/A/png/A_%%5d.png -vb %BITRATE% -auto-alt-ref %USE_AUTO_ALT_REF% -c:v libvpx %OUTPUT_DIR%UnityAnimeA.webm
+"%FFMPEG_EXE%" -r %FRAMERATE% -i C88unity-chan_animation/B/png/B_%%5d.png -vb %BITRATE% -auto-alt-ref %USE_AUTO_ALT_REF% -c:v libvpx %OUTPUT_DIR%UnityAnimeB.webm
 
 
 @PAUSE
